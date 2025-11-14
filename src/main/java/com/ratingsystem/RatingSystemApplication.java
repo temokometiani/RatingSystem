15b1eeae2c583fc4e9907816e2b2a825710e9aed
Teimuraz Kometiani <temoqometiani3002@gmail.com>
2025-11-14 14:00:54 +0400

    Initial commit

---
+++ src/main/java/com/ratingsystem/RatingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.ratingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingSystemApplication.class, args);
+    }
+}
